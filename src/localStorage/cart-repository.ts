e29afeb3470ada0ff8261da280df4802a2e30c68
Warley Dias <72942099+wwddnn@users.noreply.github.com>
2025-08-30 16:50:14 -0300

    Resolvendo problema subtotal e total, e incluido link para botão continuar comprando voltar para a tela de catalog

--- src/localStorage/cart-repository.ts
+++ src/localStorage/cart-repository.ts
@@ -1,4 +1,4 @@
-import type { OrderDTO } from "../models/order";
+import { OrderDTO, OrderItemDTO } from "../models/order";
 import { CART_KEY } from "../utils/system";
 
 export function save(cart: OrderDTO) {
@@ -8,5 +8,13 @@ export function save(cart: OrderDTO) {
 
 export function get() : OrderDTO {
     const str = localStorage.getItem(CART_KEY) || '{"items":[]}';
-    return JSON.parse(str);
+    const obj = JSON.parse(str) as OrderDTO;
+
+    const cart = new OrderDTO();
+    obj.items.forEach(x => {
+      cart.items.push(new OrderItemDTO(x.productId, x.quantity, x.name, x.price, x.imgUrl));
+    })
+
+    console.log(cart);
+    return cart
 }
--- src/routes/ClientHome/Cart/index.tsx
+++ src/routes/ClientHome/Cart/index.tsx
@@ -2,6 +2,7 @@ import "./styles.css";
 import { useState } from "react";
 import { OrderDTO } from "../../../models/order";
 import * as cartService from "../../../services/cart-service";
+import { Link } from "react-router-dom";
 
 export default function Cart() {
 
@@ -34,14 +35,14 @@ export default function Cart() {
                     </div>
                   </div>
                   <div className="dsc-cart-item-right">
-                    R$ {item.subTotal}
+                    R$ {item.subTotal.toFixed(2)}
                   </div>
                 </div>
               ))
               }
 
               <div className="dsc-cart-total-container">
-                <h3>R$ {cart.total}</h3>
+                <h3>R$ {cart.total.toFixed(2)}</h3>
               </div>
             </div>
           )
@@ -49,7 +50,9 @@ export default function Cart() {
 
         <div className="dsc-btn-page-container">
           <div className="dsc-btn dsc-btn-blue">Finalizar pedido</div>
-          <div className="dsc-btn dsc-btn-white">Continuar comprando</div>
+          <Link to="/catalog">
+            <div className="dsc-btn dsc-btn-white">Continuar comprando</div>
+          </Link>
         </div>
       </section>
     </main>
