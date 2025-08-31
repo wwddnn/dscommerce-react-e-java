ef4b4ca2012c8eac73c72d7ec0c0a042143f0f09
Warley Dias <72942099+wwddnn@users.noreply.github.com>
2025-08-31 18:03:45 -0300

    Incrementar a quantidade de um item no carrinho com funcao increaseItem no cart-service

--- src/routes/ClientHome/Cart/index.tsx
+++ src/routes/ClientHome/Cart/index.tsx
@@ -12,7 +12,13 @@ export default function Cart() {
     cartService.clearCart();
     setCart(cartService.getCart());
   }
-  
+
+  function handleIncreaseItem(productId: number) {
+    cartService.increaseItem(productId);
+    setCart(cartService.getCart());
+
+  }
+
   return (
     <main>
       <section id="cart-container-section" className="dsc-container">
@@ -35,7 +41,7 @@ export default function Cart() {
                       <div className="dsc-cart-item-quantity-container">
                         <div className="dsc-cart-item-quantity-btn">-</div>
                           <p>{item.quantity}</p>
-                        <div className="dsc-cart-item-quantity-btn">+</div>
+                        <div onClick={ () => handleIncreaseItem(item.productId)} className="dsc-cart-item-quantity-btn">+</div>
                       </div>
                     </div>
                   </div>
--- src/services/cart-service.ts
+++ src/services/cart-service.ts
@@ -30,3 +30,12 @@ export function addProduct(product: ProductDTO) {
 export function clearCart() {
     cartRepository.clear();
 }
+
+export function increaseItem(productId: number) {
+    const cart = cartRepository.get();
+    const item = cart.items.find(x => x.productId === productId);
+    if(item) {
+        item.quantity++;
+        cartRepository.save(cart);
+    }
+}
