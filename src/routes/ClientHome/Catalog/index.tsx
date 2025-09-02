84b00f658e658106160e278a386a828ea84c3ec7
Warley Dias <72942099+wwddnn@users.noreply.github.com>
2025-09-02 14:10:49 -0300

    Buscando a proxima pagina de produtos

--- src/routes/ClientHome/Catalog/index.tsx
+++ src/routes/ClientHome/Catalog/index.tsx
@@ -13,6 +13,10 @@ type QueryParams = {
 }
 
 export default function Catalog() {
+
+  const [isLastPage, setIsLastPage] = useState(false);
+
+
   const [products, setProducts] = useState<ProductDTO[]>([]);
 
   const [queryParams, setQueryParams] = useState<QueryParams>({
@@ -23,12 +27,19 @@ export default function Catalog() {
   useEffect(() => {
     productService.findPageRequest(queryParams.page, queryParams.name)
     .then((response) => {
-      setProducts(response.data.content);
+      const nextPage = response.data.content;
+      setProducts(products.concat(nextPage));
+      setIsLastPage(response.data.last);
     });
   }, [queryParams]);
 
   function handleSearch(searchText: string) {
-    setQueryParams({ ...queryParams, name: searchText });
+    setProducts([]);
+    setQueryParams({ ...queryParams, page: 0, name: searchText });
+  }
+
+  function handleNextPageClick() {
+    setQueryParams({ ...queryParams, page: queryParams.page + 1});
   }
 
   return (
@@ -44,7 +55,13 @@ export default function Catalog() {
           }
         </div>
 
-        <ButtonNextPage />
+        { 
+          !isLastPage &&
+          <div onClick={handleNextPageClick}>
+            <ButtonNextPage />
+          </div>
+        }
+
       </section>
     </main>
   );
