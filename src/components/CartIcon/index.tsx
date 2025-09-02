07bad582857c4f3371f5e9a0cb63973bf58362d6
Warley Dias <72942099+wwddnn@users.noreply.github.com>
2025-09-02 15:13:54 -0300

    Buscando os dados do localStorage para atualizar a bolinha vermelha de itens do carrinho

--- src/components/CartIcon/index.tsx
+++ src/components/CartIcon/index.tsx
@@ -1,13 +1,15 @@
-
-import './styles.css';
-import cartIcon from '../../assets/cart.svg';
-
+import "./styles.css";
+import cartIcon from "../../assets/cart.svg";
+import { useState } from "react";
+import * as cartService from "../../services/cart-service";
 
 export default function CartIcon() {
+  const [cart, setCart] = useState(cartService.getCart());
+
   return (
     <>
       <img src={cartIcon} alt="Carrinho de compras" />
-      <div className="dsc-cart-count">2</div>
+      <div className="dsc-cart-count">{cart.items.length}</div>
     </>
   );
 }
