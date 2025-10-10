3c6df4b7729205079628954f14d28388c86e324f
Warley Dias <72942099+wwddnn@users.noreply.github.com>
2025-10-10 10:53:56 -0300

    Controle de acesso em nivel de rota, atualizando PrivateRoutes com props roles

--- src/components/PrivateRoute/index.tsx
+++ src/components/PrivateRoute/index.tsx
@@ -1,14 +1,19 @@
 import { Navigate } from "react-router-dom";
 import * as authService from '../../services/auth-service';
+import type { RoleEnum } from "../../models/auth";
 
 // children permite abrir o componente colocou outros componentes dentro dele 
 // esse children que é passado como proprs é o <Admin /> que esta la no app.tsx e esta dentro do PrivateRoute lá
 type Props = {
   children: JSX.Element;
+  roles?: RoleEnum[];
 };
-export function PrivateRoute({ children }: Props) {
+export function PrivateRoute({ children, roles = [] }: Props) {
   if (!authService.isAuthenticated()) {
     return <Navigate to="/login" />;
+  }
+  if(!authService.hasAnyRoles(roles)) {
+    return <Navigate to="/catalog" />;
   }
   return children;
 }
