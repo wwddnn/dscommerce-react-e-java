f90fdffcd550467af8770b71c73b307ef43e449b
Warley Dias <72942099+wwddnn@users.noreply.github.com>
2025-10-17 06:41:01 -0300

    Criacao de objeto queryParams no catalog

--- src/routes/ClientHome/Catalog/index.tsx
+++ src/routes/ClientHome/Catalog/index.tsx
@@ -6,21 +6,29 @@ import { useEffect, useState } from "react";
 import type { ProductDTO } from "../../../models/product";
 import * as productService from "../../../services/product-service";
 
+type QueryParams = {
+  page: number;
+  name: string;
+}
+
 export default function Catalog() {
 
   const [products, setProducts] = useState<ProductDTO[]>([]);
 
-  const [productName, setProductName] = useState("");
+  const [queryParams, setQueryParams] = useState<QueryParams>({
+    page: 0,
+    name: ""
+  });
 
   useEffect(() => {
-    productService.findPageRequest(0, productName)
+    productService.findPageRequest(queryParams.page, queryParams.name)
     .then(response => {
       setProducts(response.data.content);
     });
-  }, [productName]);
+  }, [queryParams]);
 
   function handleSearch(searchText: string) {
-    setProductName(searchText);
+    setQueryParams({...queryParams, name: searchText});
   }
 
   return (
