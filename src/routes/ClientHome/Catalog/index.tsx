4b429c403ee81389b4089e721585e2a73fbb101f
Warley Dias <72942099+wwddnn@users.noreply.github.com>
2025-10-17 07:28:59 -0300

    Buscar a proxima pagina de produtos e retirar botao de proxima pagina se for a ultima pagina

--- src/routes/ClientHome/Catalog/index.tsx
+++ src/routes/ClientHome/Catalog/index.tsx
@@ -13,6 +13,8 @@ type QueryParams = {
 
 export default function Catalog() {
 
+  const [isLastPage, setIsLastPage] = useState();
+
   const [products, setProducts] = useState<ProductDTO[]>([]);
 
   const [queryParams, setQueryParams] = useState<QueryParams>({
@@ -23,12 +25,19 @@ export default function Catalog() {
   useEffect(() => {
     productService.findPageRequest(queryParams.page, queryParams.name)
     .then(response => {
-      setProducts(response.data.content);
+      const nextPage = response.data.content;
+      setProducts(products.concat(nextPage));
+      setIsLastPage(response.data.last);
     });
   }, [queryParams]);
 
   function handleSearch(searchText: string) {
-    setQueryParams({...queryParams, name: searchText});
+    setProducts([]);
+    setQueryParams({...queryParams, page: 0, name: searchText});
+  }
+
+  function handleNextPageClick() {
+    setQueryParams({...queryParams, page: queryParams.page + 1});
   }
 
   return (
@@ -42,7 +51,12 @@ export default function Catalog() {
           ))}
         </div>
 
-        <ButtonNextPage />
+          {
+            !isLastPage &&
+              <div onClick={handleNextPageClick} >
+                <ButtonNextPage />
+              </div>
+          }
       </section>
     </main>
   );
