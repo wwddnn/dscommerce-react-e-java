527b269a784ba2059e118abb5089ce740ffab289
Warley Dias <72942099+wwddnn@users.noreply.github.com>
2025-10-28 18:38:41 -0300

    Controle de rota, atualizando o PrivateRoute e tambem o apptsx

--- src/components/PrivateRoute/index.tsx
+++ src/components/PrivateRoute/index.tsx
@@ -1,14 +1,21 @@
-import * as authService from '../../services/auth-service';
-import { Navigate } from 'react-router-dom';
+import type { RoleEnum } from "../../models/auth";
+import * as authService from "../../services/auth-service";
+import { Navigate } from "react-router-dom";
 
 type Props = {
   children: JSX.Element;
+  roles?: RoleEnum[];
 };
 
-// se o usuário não estiver autenticado então vai direcionar para a página de Login, caso contrário retorna o children
-export function PrivateRoute({ children }: Props) {
+// se o usuário não estiver autenticado então vai direcionar para a página de Login
+// se o usuário não tiver os papéis (roles) necessários então vai direcionar para a página de catálogo
+// se passar nos dois casos, ai vou mostrar o conteúdo protegido
+export function PrivateRoute({ children, roles = [] }: Props) {
   if (!authService.isAuthenticated()) {
     return <Navigate to="/login" />;
+  }
+  if (!authService.hasAnyRoles(roles)) {
+    return <Navigate to="/catalog" />;
   }
   return children;
 }
