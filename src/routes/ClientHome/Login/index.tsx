33cae89a2c77c7290f9843ad9e4e426b2f2bd443
Warley Dias <72942099+wwddnn@users.noreply.github.com>
2025-11-14 14:43:28 -0300

    Novo formato para os dados do input do Login. Criacao do componente FormInput para desestruturar os dados que vem do formulario

--- src/routes/ClientHome/Login/index.tsx
+++ src/routes/ClientHome/Login/index.tsx
@@ -1,77 +1,91 @@
-import './styles.css';
-import { useContext, useState } from 'react';
-import type { CredentialsDTO } from '../../../models/auth';
-import * as authService from '../../../services/auth-service';
-import { useNavigate } from 'react-router-dom';
-import { ContextToken } from '../../../utils/context-token';
+import "./styles.css";
+import { useContext, useState } from "react";
+import type { CredentialsDTO } from "../../../models/auth";
+import * as authService from "../../../services/auth-service";
+import { useNavigate } from "react-router-dom";
+import { ContextToken } from "../../../utils/context-token";
+import FormInput from "../../../components/FormInput";
 
 export default function Login() {
+  const navigate = useNavigate();
 
-    const navigate = useNavigate();
+  const { setContextTokenPayload } = useContext(ContextToken);
 
-    const { setContextTokenPayload } = useContext(ContextToken);
+  const [formData, setFormData] = useState<any>({
+    username: {
+      value: "", // valor é passado como zero no início
+      id: "username",
+      name: "username",
+      type: "text",
+      placeholder: "Email",
+      validation: function (value: string) {
+        return /^[a-zA-Z0-9.!#$%&'*+/=?^_`{|}~-]+@[a-zA-Z0-9-]+(?:\.[a-zA-Z0-9-]+)*$/.test(
+          value.toLowerCase()
+        );
+      },
+      message: "Favor informar um email válido",
+    },
+    password: {
+      value: "", // valor é passado como zero no início
+      id: "password",
+      name: "password",
+      type: "password",
+      placeholder: "Senha",
+    },
+  });
 
-    const [formData, setFormData] = useState<CredentialsDTO>({
-        username: '',
-        password: ''
-    });
+  function handleSubmit(event: any) {
+    event.preventDefault();
+    authService
+      .loginRequest({username: formData.username.value, password: formData.password.value})
+      .then((response) => {
+        authService.saveAccessToken(response.data.access_token);
+        setContextTokenPayload(authService.getAccessTokenPayload());
+        navigate("/catalog");
+      })
+      .catch((error) => {
+        console.log("Erro no login", error);
+      });
+  }
 
-    function handleSubmit(event: any) {
-        event.preventDefault();
-        authService.loginRequest(formData)
-            .then(response => {
-                authService.saveAccessToken(response.data.access_token);
-                setContextTokenPayload(authService.getAccessTokenPayload());
-                navigate('/catalog');
+  function handleInputChange(event: any) {
+    const name = event.target.name;
+    const value = event.target.value;
+    setFormData({ ...formData, [name]: {...formData[name], value: value} });
+  }
 
-            })
-            .catch(error => {
-                console.log("Erro no login", error);
-            })
-    }
+  return (
+    <main>
+      <section id="login-section" className="dsc-container">
+        <div className="dsc-login-form-container">
+          <form className="dsc-card dsc-form" onSubmit={handleSubmit}>
+            <h2>Login</h2>
+            <div className="dsc-form-controls-container">
+              <div>
+                <FormInput
+                {...formData.username}
+                  className="dsc-form-control"
+                  onChange={handleInputChange}
+                />
+                <div className="dsc-form-error"></div>
+              </div>
+              <div>
+                <FormInput
+                    {...formData.password}
+                  className="dsc-form-control"
+                  onChange={handleInputChange}
+                />
+              </div>
+            </div>
 
-    function handleInputChange(event: any) {
-        const name = event.target.name;
-        const value = event.target.value;
-        setFormData({ ...formData, [name]: value });
-    }
-
-    return(
-        <main>
-            <section id="login-section" className="dsc-container">
-                <div className="dsc-login-form-container">
-                    <form className="dsc-card dsc-form" onSubmit={handleSubmit}>
-                        <h2>Login</h2>
-                            <div className="dsc-form-controls-container">
-                                <div>
-                                    <input 
-                                        name="username"
-                                        value={formData.username}
-                                        className="dsc-form-control" 
-                                        type="text" 
-                                        placeholder="Email"
-                                        onChange={handleInputChange} 
-                                    />
-                                    <div className="dsc-form-error"></div>
-                                </div>
-                                <div>
-                                    <input 
-                                        name="password"
-                                        value={formData.password}
-                                        className="dsc-form-control" 
-                                        type="password" 
-                                        placeholder="Senha"
-                                        onChange={handleInputChange} 
-                                    />
-                                </div>
-                            </div>
-
-                        <div className="dsc-login-form-buttons dsc-mt20">
-                        <button type="submit" className="dsc-btn dsc-btn-blue">Entrar</button>
-                        </div>
-                    </form>
-                </div>
-            </section>
-        </main>
-    );
+            <div className="dsc-login-form-buttons dsc-mt20">
+              <button type="submit" className="dsc-btn dsc-btn-blue">
+                Entrar
+              </button>
+            </div>
+          </form>
+        </div>
+      </section>
+    </main>
+  );
 }
