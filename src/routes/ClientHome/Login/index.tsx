dde197e6ac38167254a63038784cb8e8d2b1bf81
Warley Dias <72942099+wwddnn@users.noreply.github.com>
2025-11-14 15:05:57 -0300

    Criacao da funcao update para atualizar os dados do formulario

--- src/routes/ClientHome/Login/index.tsx
+++ src/routes/ClientHome/Login/index.tsx
@@ -1,12 +1,13 @@
 import "./styles.css";
 import { useContext, useState } from "react";
-import type { CredentialsDTO } from "../../../models/auth";
 import * as authService from "../../../services/auth-service";
 import { useNavigate } from "react-router-dom";
 import { ContextToken } from "../../../utils/context-token";
 import FormInput from "../../../components/FormInput";
+import * as forms from "../../../utils/forms";
 
 export default function Login() {
+
   const navigate = useNavigate();
 
   const { setContextTokenPayload } = useContext(ContextToken);
@@ -51,7 +52,7 @@ export default function Login() {
   function handleInputChange(event: any) {
     const name = event.target.name;
     const value = event.target.value;
-    setFormData({ ...formData, [name]: {...formData[name], value: value} });
+    setFormData(forms.update(formData, name, value));
   }
 
   return (
