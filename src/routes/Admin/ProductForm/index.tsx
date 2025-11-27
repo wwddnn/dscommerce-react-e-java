4f669c78c0fe98a7eaedbd7e9d739ffc11f1f209
Warley Dias <72942099+wwddnn@users.noreply.github.com>
2025-11-27 07:42:52 -0300

    Estilizando o FormSelect e cada atributo dele, usando site react select para quais atributos preciso alterar

--- src/routes/Admin/ProductForm/index.tsx
+++ src/routes/Admin/ProductForm/index.tsx
@@ -8,6 +8,7 @@ import FormTextArea from "../../../components/FormTextArea";
 import type { CategoryDTO } from "../../../models/category";
 import * as categoryService from "../../../services/category";
 import FormSelect from "../../../components/FormSelect";
+import { selectStyles } from "../../../utils/select";
 
 export default function ProductForm() {
   const params = useParams();
@@ -135,7 +136,8 @@ export default function ProductForm() {
               <div>
                 <FormSelect
                   {...formData.categories}
-                  className="dsc-form-control"
+                  className="dsc-form-control dsc-form-select-container"
+                  styles={selectStyles}
                   options={categories}
                   onChange={(obj: any) => {
                     const newFormData = forms.updateAndValidate(formData, "categories", obj);
---
+++ src/utils/select.ts
@@ -0,0 +1,25 @@
+
+/* estilizando o FormSelect, ver no site react select style*/
+export const selectStyles = {
+    control: (provided: any) => ({
+        ...provided,
+        minHeight: "40px",
+        border: "none",
+        boxShadow: "none",
+        "&:hover": {
+            border: "none",
+        },
+    }),
+    placeholder: (provided: any) => ({
+        ...provided,
+        color: "var(--dsc-color-font-placeholder)",
+    }),
+    option: (provided: any) => ({
+        ...provided,
+        color: "var(--dsc-color-font-primary)",
+    }),
+    indicatorSeparator: (provided: any) => ({
+        ...provided,
+        display: "none",
+    }),
+};
