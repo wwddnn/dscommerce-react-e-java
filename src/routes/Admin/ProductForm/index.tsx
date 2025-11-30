d8b637c91bac3539ddcfb900cb028c0f7d3ead1d
Warley Dias <72942099+wwddnn@users.noreply.github.com>
2025-11-30 09:30:07 -0300

    Criacao da funcao setBackendErrors no forms ts, e usando essa funcao para buscar os erros do back end e suas mensagens no ProductForm conforme tenta salvar um produto faltando o preenchimentos dos campos obrigatorios

--- src/routes/Admin/ProductForm/index.tsx
+++ src/routes/Admin/ProductForm/index.tsx
@@ -119,6 +119,10 @@ export default function ProductForm() {
     request
       .then(() => {
         navigate("/admin/products");
+    })
+    .catch((error) => {
+      const newInputs = forms.setBackendErrors(formData, error.response.data.errors);
+      setFormData(newInputs);
     });
   }
 
